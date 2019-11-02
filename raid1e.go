3702d0eee83cdaeb34c0f25d588bf81f375569b0
Topexan <54759948+Topexan@users.noreply.github.com>
2019-11-02 19:28:28 +0600

    raid1e

---
+++ raid1e.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func strtoNum(s string) int {
+	runes := []rune(s)
+	num := 0
+	for i := range runes {
+		num *= 10
+		if runes[i] == '1' {
+			num = num + 1
+		} else if runes[i] == '2' {
+			num = num + 2
+		} else if runes[i] == '3' {
+			num = num + 3
+		} else if runes[i] == '4' {
+			num = num + 4
+		} else if runes[i] == '5' {
+			num = num + 5
+		} else if runes[i] == '6' {
+			num = num + 6
+		} else if runes[i] == '7' {
+			num = num + 7
+		} else if runes[i] == '8' {
+			num = num + 8
+		} else if runes[i] == '9' {
+			num = num + 9
+		} else if runes[i] == '0' {
+			num = num + 0
+		}
+	}
+	return num
+}
+
+func Raid1e(x, y int) {
+	if y >= 1 {
+		if x > 0 {
+			z01.PrintRune(65)
+		}
+		for i := 1; i <= x-2; i++ {
+			z01.PrintRune(66)
+		}
+		if x > 1 {
+			z01.PrintRune(67)
+		}
+		if x > 0 {
+			z01.PrintRune(10)
+		}
+
+	}
+
+	if y > 1 {
+		if x > 0 {
+			for j := 1; j <= y-2; j++ {
+				z01.PrintRune(66)
+				for i := 1; i <= x-2; i++ {
+					z01.PrintRune(32)
+				}
+				if x > 1 {
+					z01.PrintRune(66)
+				}
+				z01.PrintRune(10)
+			}
+		}
+	}
+
+	if y > 1 {
+		if x > 0 {
+			z01.PrintRune(67)
+		}
+		for i := 1; i <= x-2; i++ {
+			z01.PrintRune(66)
+		}
+		if x > 1 {
+			z01.PrintRune(65)
+		}
+		if x > 0 {
+			z01.PrintRune(10)
+		}
+	}
+}
+
+func main() {
+	arguments := os.Args
+	l := len(arguments)
+	if l == 3 {
+		Raid1e(strtoNum(arguments[1]), strtoNum(arguments[2]))
+	}
+}
